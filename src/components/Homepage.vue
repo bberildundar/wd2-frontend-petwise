044e85664565727985723963b895983cfe540d31
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-06-19 23:50:44 +0200

    added backend connection, made vet items dynamic

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,84 +1,76 @@
 <template>
     <section>
         <div class="jumbotron jumbotron-fluid"
-     style="background-image: url('/images/homepage-banner.jpg'), url('https://picsum.photos/800/600'); padding-top: 300px; padding-bottom: 100px;">
-    <div class="container">
-        <h1 class="display-4 fw-semibold">Welcome to PetWise!</h1>
-        <p class="lead fw-semibold">Where Your Pet's Health Is Our Top Priority</p>
+            style="background-image: url('/images/homepage-banner.jpg'), url('https://picsum.photos/800/600'); padding-top: 300px; padding-bottom: 100px;">
+            <div class="container">
+                <h1 class="display-4 fw-semibold">Welcome to PetWise!</h1>
+                <p class="lead fw-semibold">Where Your Pet's Health Is Our Top Priority</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2 class="mt-4" id="home-veterinarians-header">Veterinarians</h2>
+            <p>At PetWise, we have a team of highly skilled veterinarians who are passionate about providing exceptional
+                care
+                for your beloved pets.
+                Our veterinarians are dedicated to ensuring the health and well-being of your furry friends.</p>
+
+                <div class="row">
+      <vet-item v-for="vet in vets" :key="vet.id" :vet="vet" />
     </div>
-</div>
 
-<div class="container">
-    <h2 class="mt-4" id="home-veterinarians-header">Veterinarians</h2>
-    <p>At PetWise, we have a team of highly skilled veterinarians who are passionate about providing exceptional care
-        for your beloved pets.
-        Our veterinarians are dedicated to ensuring the health and well-being of your furry friends.</p>
+            <div class="row mt-5">
+                <div class=" col">
+                    <h2 class="mt-4" id="home-address-and-contact-header">Address</h2>
+                    <p>PetWise Veterinary Clinic <br>
+                        Koningstraat 10 <br>
+                        1234 AB Amsterdam</p>
+                    <h2 class="mt-4">Contact Us</h2>
+                    <p>Phone: (123) 456-7890 <br>
+                        Email: info@petwisevetclinic.nl</p>
+                </div>
+                <div class=" col">
+                    <img src="/images/address-image.jpg" class="img-fluid" alt="cat-image">
+                </div>
+            </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="card m-1">
-                <img src="/images/vet-1.jpg" class="card-img-top" alt="doctor-image">
-                <div class="card-body">
-                    <h4>Dr. Emily Nguyen</h4>
-                    <p class="card-text">Specializes in Orthopedic Surgery</p>
-                </div>
-            </div>
         </div>
-        <div class="col">
-            <div class="card m-1">
-                <img src="/images/vet-2.jpg" class="card-img-top" alt="doctor-image">
-                <div class="card-body">
-                    <h4>Dr. Samantha Patel</h4>
-                    <p class="card-text">Specializes in Canine Oncology</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card m-1">
-                <img src="/images/vet-3.jpg" class="card-img-top" alt="doctor-image">
-                <div class="card-body">
-                    <h4>Dr. Eric Chavez</h4>
-                    <p class="card-text">Specializes in Feline Medicine</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card m-1">
-                <img src="/images/vet-4.jpg" class="card-img-top" alt="doctor-image">
-                <div class="card-body">
-                    <h4>Dr. Michael Rodriguez</h4>
-                    <p class="card-text">Specializes in Small Animal Surgery</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row mt-5">
-            <div class=" col">
-                <h2 class="mt-4" id="home-address-and-contact-header">Address</h2>
-                <p>PetWise Veterinary Clinic <br>
-                    Koningstraat 10 <br>
-                    1234 AB Amsterdam</p>
-                <h2 class="mt-4">Contact Us</h2>
-                <p>Phone: (123) 456-7890 <br>
-                    Email: info@petwisevetclinic.nl</p>
-            </div>
-            <div class=" col">
-                <img src="/images/address-image.jpg" class="img-fluid" alt="cat-image">
-            </div>
-        </div>
-    </div>
-</div>
     </section>
-  </template>
+</template>
   
-  <script>
-  export default {
-    name: "Homepage",
-  };
-  </script>
+<script>
+import axios from '../axios-auth';
+import VetItem from './vets/vetItem.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    VetItem,
+  },
+  data() {
+    return {
+      vets: [],
+    };
+  },
+  mounted() {
+    this.update();
+  },
+  methods: {
+    update() {
+      axios
+        .get("http://localhost/vets")
+        .then((result) => {
+          console.log(result);
+          this.vets = result.data;
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+};
+</script>
   
-  <style>
-  .h2{
+<style>
+.h2 {
     font-size: 99;
-  }
-  </style>
+}
+</style>
---
+++ src/components/vets/vetItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col">
+    <div class="card m-1">
+        <img :src="vet.imageURL" class="card-img-top" alt="doctor-image">
+        <div class="card-body">
+            <h4>Dr. {{vet.firstName}} {{ vet.lastName }}</h4>
+            <p class="card-text">Specializes in {{ vet.specialization }}</p>
+        </div>
+    </div></div>
+</template>
+  
+<script>
+export default {
+    name: "VetItem",
+    props: {
+        vet: Object,
+    },
+    methods: {
+
+    }
+};
+</script>
+  
+<style></style>
